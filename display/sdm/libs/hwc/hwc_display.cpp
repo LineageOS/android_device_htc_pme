447657ec00c51d1284700d18303d8e4f12504b3d
Scott Mertz <scott@cyngn.com>
2015-12-22 10:57:54 -0800

    msm8996: remove unused qcom features
    CM sets QCOM_BSP globally, but CAF sets it per project.  In CAF hwc,
    QCOM_BSP isn't actually set at all.  Align our fork with CAF by
    ifdeffing the offending features.
    
    Change-Id: Id096d2468974ed3983d2bcb2eef728eb11ef1e56

--- display/sdm/libs/hwc/hwc_display.cpp
+++ display/sdm/libs/hwc/hwc_display.cpp
@@ -47,7 +47,7 @@
 
 #define __CLASS__ "HWCDisplay"
 
-#ifdef QCOM_BSP
+#if 0
 #include <exhwcomposer_defs.h>
 #endif
 
@@ -569,7 +569,7 @@ int HWCDisplay::PrePrepareLayerStack(hwc_display_contents_1_t *content_list) {
       LayerCache layer_cache = layer_stack_cache_.layer_cache[i];
       layer.flags.updating = IsLayerUpdating(hwc_layer, layer_cache);
     }
-#ifdef QTI_BSP
+#if 0
     if (hwc_layer.flags & HWC_SCREENSHOT_ANIMATOR_LAYER) {
       layer_stack_.flags.animating = true;
     }
