230a073023f03621cfe1bcfc4148bec27cd49e8c
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-05-21 01:16:32 -0400

    pme: build Gello
    Change-Id: I7e88fe98f3e36e4c6eff68d616305ca117f6caba

--- device.mk
+++ device.mk
@@ -86,6 +86,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# Browser
+PRODUCT_PACKAGES += \
+    Gello
+
 # Camera
 PRODUCT_PACKAGES += \
     libxml2 \
