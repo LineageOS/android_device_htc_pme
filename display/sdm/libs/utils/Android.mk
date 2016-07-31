3d8bf34aea5e98a0e4515fd199ae85dff3f3e99f
Christopher R. Palmer <crpalmer@gmail.com>
2016-07-31 08:13:55 -0400

    pme: display: Import LA.HB.1.3.1_rb1.5
    Change-Id: Ibb2f461b51996b67fad82b8a873b118e72543839

---
+++ display/sdm/libs/utils/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE                  := libsdmutils
+LOCAL_MODULE_TAGS             := optional
+LOCAL_C_INCLUDES              := hardware/qcom/display/sdm/include/
+LOCAL_CFLAGS                  := -Wno-missing-field-initializers \
+                                 -Wall -Werror -std=c++11 -fcolor-diagnostics\
+                                 -DLOG_TAG=\"SDM\"
+LOCAL_CLANG                   := true
+LOCAL_SRC_FILES               := debug.cpp \
+                                 rect.cpp \
+                                 sys.cpp
+
+include $(BUILD_SHARED_LIBRARY)
