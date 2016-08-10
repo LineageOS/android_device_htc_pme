672b46b17fb3e1a81874a3a4abfb710608eedc3d
Christopher R. Palmer <crpalmer@gmail.com>
2016-08-10 05:34:14 -0400

    WIP: pme: Add an amplifier HAL
    Change-Id: Ib0f1d348a83761c3f452980e0d5df26a9e45e8f2

--- amplifier/Android.mk
+++ amplifier/Android.mk
@@ -1,8 +1,41 @@
 LOCAL_PATH := $(call my-dir)
 
+#include $(CLEAR_VARS)
+#
+#LOCAL_SHARED_LIBRARIES := \
+#	liblog libutils libcutils libtinyalsa
+#
+#LOCAL_C_INCLUDES := \
+#        external/tinyalsa/include \
+#	hardware/libhardware/include
+#
+#LOCAL_SRC_FILES := \
+#	audio_amplifier.c tfa.c
+#
+#LOCAL_MODULE := audio_amplifier.msm8996
+#LOCAL_MODULE_RELATIVE_PATH := hw
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_CFLAGS = -Werror
+#
+#include $(BUILD_SHARED_LIBRARY)
+
 include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := \
+	liblog libutils libcutils libtinyalsa
+
+LOCAL_C_INCLUDES := \
+        external/tinyalsa/include \
+	hardware/libhardware/include
+
+LOCAL_CFLAGS := -g -O0
+
 LOCAL_SRC_FILES := \
-	amp_loader.c
-LOCAL_CFLAGS := -Wall -Wextra -std=c11
-LOCAL_MODULE := amp_loader
+	play.c tfa9888-debug.c tfa.c tfa-cont.c
+
+LOCAL_MODULE := play
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS = -Werror
+
 include $(BUILD_EXECUTABLE)
+
