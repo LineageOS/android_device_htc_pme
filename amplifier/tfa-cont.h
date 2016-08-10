ff295c63b3479d051e89dd04597d401689f2a86f
Christopher R. Palmer <crpalmer@gmail.com>
2016-08-10 05:34:14 -0400

    WIP: pme: Add an amplifier HAL
    Change-Id: Ib0f1d348a83761c3f452980e0d5df26a9e45e8f2

---
+++ amplifier/tfa-cont.h
@@ -0,0 +1,23 @@
+#ifndef __TFA_CONT_H__
+#define __TFA_CONT_H__
+
+typedef struct tfa_contS tfa_cont_t;
+
+#include "tfa.h"
+
+tfa_cont_t *tfa_cont_new(const char *fname);
+void tfa_cont_destroy(tfa_cont_t *);
+int tfa_cont_get_cal_profile(tfa_cont_t *tc);
+const char *tfa_cont_get_profile_name(tfa_cont_t *tc, int profile);
+
+int tfa_cont_write_device_files(tfa_cont_t *, tfa_t *);
+int tfa_cont_write_patch(tfa_cont_t *tc, tfa_t *t);
+int tfa_cont_write_profile(tfa_cont_t *tc, int prof_num, int vstep, tfa_t *t);
+int tfa_cont_write_profile_files(tfa_cont_t *tc, int profile_num, tfa_t *t, int vstep);
+
+int tfa_cont_write_device_registers(tfa_cont_t *tc, tfa_t *t);
+int tfa_cont_write_profile_registers(tfa_cont_t *tc, int prof_num, tfa_t *t);
+
+int tfa_cont_get_current_vstep(tfa_cont_t *t);
+
+#endif
