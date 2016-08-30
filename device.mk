e7b9e27df4842ceab9ee4041a7355604a09e8386
Steve Kondik <steve@cyngn.com>
2016-08-30 10:40:44 -0700

    pme: Import GPS hal
     * Bring in latest UM HAL
     * Requires updated proprietary bits
    
    Change-Id: I6b0f1ce90d79fded7882892d00022a5b69d44fc9

--- device.mk
+++ device.mk
@@ -121,10 +121,17 @@ PRODUCT_PACKAGES += \
     libtinyxml
 
 # GPS
+PRODUCT_PACKAGES += \
+    gps.msm8996 \
+    libcurl
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:system/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
 # HTC Logging functions
 PRODUCT_PACKAGES += \
