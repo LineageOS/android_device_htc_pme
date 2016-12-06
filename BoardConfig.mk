1321dafecdb0f89143f7f2a17cf9554070a3fb96
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-12-06 19:09:24 +0000

    pme: update camera blobs
    * add missing adsp blobs
    * remove camera flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,6 @@ BOARD_CUSTOM_BT_CONFIG := $(DEVICE_PATH)/bluetooth/libbt_vndcfg.txt
 # Camera
 TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY := libcamera_parameters_ext
 USE_DEVICE_SPECIFIC_CAMERA := true
-TARGET_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND='{"htc.camera.sensor.", AID_CAMERA, 0}, {"camera.4k2k.", AID_MEDIA, 0},'
 
 # CMHW
 BOARD_USES_CYANOGEN_HARDWARE := true
