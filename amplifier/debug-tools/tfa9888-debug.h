3b87ece1823ba614804789e980e794332515eaf6
Christopher R. Palmer <crpalmer@gmail.com>
2016-12-14 04:38:25 -0500

    pme: amplifier: Update the amplifier for N's kernel based driver
    Change-Id: Ia46ad13c48e04106ed1305758e5729b3740c3b50

--- amplifier/debug-tools/tfa9888-debug.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TFA9888_DEBUG_H__
-#define __TFA9888_DEBUG_H__
-
-#include <stdio.h>
-
-void tfa9888_print_bitfield(FILE *f, unsigned reg, unsigned v);
-
-#endif
--- amplifier/tfa9888-debug.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TFA9888_DEBUG_H__
-#define __TFA9888_DEBUG_H__
-
-char *tfa9888_reg_to_string(unsigned reg, unsigned v);
-
-#endif
