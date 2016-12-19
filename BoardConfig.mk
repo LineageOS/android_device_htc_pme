2fde5f959d227970992fc59db75ef8136f19722e
Christopher R. Palmer <crpalmer@gmail.com>
2016-12-18 21:03:39 -0500

    pme: ril: Remove custom ril
    The only diff from our "custom" ril derived from hiae is the addition
    of a symbol that can be trivially shimmed.  Undo the custom ril and
    add the shim in a subsequent commit.
    
    Change-Id: I0e7c4eca254cbaaacb047e1efd0a4a1768ca7c17

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,7 @@ BOARD_USES_QC_TIME_SERVICES := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # RIL
-BOARD_PROVIDES_LIBRIL := true
+TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
