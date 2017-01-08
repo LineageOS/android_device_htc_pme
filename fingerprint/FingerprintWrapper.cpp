ab89c17fbc2d2ec9d32fb52150da16257bd5eb94
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-08 07:51:08 -0500

    pme: Add a dummy fingerprint wrapper HAL
    All this does is delegate all the requests to the real HAL.
    
    Change-Id: I0510271a6b867204aad93dda8dcd44dcdac82b49

---
+++ fingerprint/FingerprintWrapper.cpp
@@ -0,0 +1,590 @@
+/*
+ * Copyright (C) 2017, The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//#define LOG_NDEBUG 0
+#define LOG_TAG "FingerprintWrapper"
+
+#include <cutils/log.h>
+
+#include <hardware/hardware.h>
+#include <hardware/fingerprint.h>
+#include <utils/threads.h>
+
+typedef struct {
+    fingerprint_device_t base;
+    union {
+        fingerprint_device_t *device;
+        hw_device_t *hw_device;
+    } vendor;
+} device_t;
+
+static android::Mutex vendor_mutex;
+
+static union {
+    fingerprint_module_t *module;
+    const hw_module_t *hw_module;
+} vendor;
+
+static bool ensure_vendor_module_is_loaded(void)
+{
+    android::Mutex::Autolock lock(vendor_mutex);
+
+    if (!vendor.module) {
+        int rv = hw_get_module_by_class("fingerprint", "vendor", &vendor.hw_module);
+        if (rv) {
+            ALOGE("failed to open vendor module, error %d", rv);
+            vendor.module = NULL;
+        } else {
+            ALOGI("loaded vendor module: %s version %x", vendor.module->common.name,
+                vendor.module->common.module_api_version);
+        }
+    }
+
+    return vendor.module != NULL;
+}
+
+static int set_notify(struct fingerprint_device *dev, fingerprint_notify_t notify)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->set_notify(device->vendor.device, notify);
+}
+
+static uint64_t pre_enroll(struct fingerprint_device *dev)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->pre_enroll(device->vendor.device);
+}
+
+static int enroll(struct fingerprint_device *dev, const hw_auth_token_t *hat, uint32_t gid,
+                uint32_t timeout_sec)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->enroll(device->vendor.device, hat, gid, timeout_sec);
+}
+
+static int post_enroll(struct fingerprint_device *dev)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->post_enroll(device->vendor.device);
+}
+
+static uint64_t get_authenticator_id(struct fingerprint_device *dev)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->get_authenticator_id(device->vendor.device);
+}
+
+static int cancel(struct fingerprint_device *dev)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->cancel(device->vendor.device);
+}
+
+static int enumerate(struct fingerprint_device *dev)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->enumerate(device->vendor.device);
+}
+
+static int remove(struct fingerprint_device *dev, uint32_t gid, uint32_t fid)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->remove(device->vendor.device, gid, fid);
+}
+
+static int set_active_group(struct fingerprint_device *dev, uint32_t gid, const char *store_path)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->set_active_group(device->vendor.device, gid, store_path);
+}
+
+static int authenticate(struct fingerprint_device *dev, uint64_t operation_id, uint32_t gid)
+{
+    device_t *device = (device_t *) dev;
+
+    return device->vendor.device->authenticate(device->vendor.device, operation_id, gid);
+}
+
+static int device_close(hw_device_t *hw_device)
+{
+    device_t *device = (device_t *) hw_device;
+    int rv = device->base.common.close(device->vendor.hw_device);
+    free(device);
+    return rv;
+}
+
+static int device_open(const hw_module_t *module, const char *name, hw_device_t **device_out)
+{
+    int rv = 0;
+    device_t *device = NULL;
+
+    if (!ensure_vendor_module_is_loaded()) {
+        return -EINVAL;
+    }
+
+    device = (device_t *) calloc(sizeof(*device), 1);
+    if (!device) {
+        ALOGE("%s: Failed to allocate memory", __func__);
+        return -ENOMEM;
+    }
+
+    rv = vendor.module->common.methods->open(vendor.hw_module, name, &device->vendor.hw_device);
+    if (rv) {
+        ALOGE("%s: failed to open, error %d\n", __func__, rv);
+        free(device);
+        return rv;
+    }
+
+    device->base.common.tag = HARDWARE_DEVICE_TAG;
+    device->base.common.version = device->vendor.device->common.version;
+    device->base.common.module = (hw_module_t *) module;
+    device->base.common.close = device_close;
+
+    device->base.set_notify = set_notify;
+    device->base.pre_enroll = pre_enroll;
+    device->base.enroll = enroll;
+    device->base.post_enroll = post_enroll;
+    device->base.get_authenticator_id = get_authenticator_id;
+    device->base.cancel = cancel;
+    device->base.enumerate = enumerate;
+    device->base.remove = remove;
+    device->base.set_active_group = set_active_group;
+    device->base.authenticate = authenticate;
+
+    *device_out = (hw_device_t *) device;
+    return 0;
+}
+
+static struct hw_module_methods_t module_methods = {
+    .open = device_open
+};
+
+fingerprint_module_t HAL_MODULE_INFO_SYM = {
+    .common = {
+        .tag = HARDWARE_MODULE_TAG,
+        .module_api_version = FINGERPRINT_MODULE_API_VERSION_2_1,
+        .hal_api_version = HARDWARE_HAL_API_VERSION,
+        .id = FINGERPRINT_HARDWARE_MODULE_ID,
+        .name = "Lineage Fingerprint Wrapper",
+        .author = "The LineageOS Project",
+        .methods = &module_methods,
+        .dso = NULL, /* remove compilation warnings */
+        .reserved = {0}, /* remove compilation warnings */
+    },
+};
+
+#if 0
+typedef struct wrapper_camera_device {
+    camera_device_t base;
+    int id;
+    camera_device_t *vendor;
+} wrapper_camera_device_t;
+
+#define VENDOR_CALL(device, func, ...) ({ \
+    wrapper_camera_device_t *__wrapper_dev = (wrapper_camera_device_t*) device; \
+    __wrapper_dev->vendor->ops->func(__wrapper_dev->vendor, ##__VA_ARGS__); \
+})
+
+#define CAMERA_ID(device) (((wrapper_camera_device_t *)(device))->id)
+
+static int check_vendor_module()
+{
+    int rv = 0;
+    ALOGV("%s", __FUNCTION__);
+
+    if (gVendorModule)
+        return 0;
+
+    rv = hw_get_module_by_class("camera", "vendor",
+            (const hw_module_t**)&gVendorModule);
+    if (rv)
+        ALOGE("failed to open vendor camera module", __FUNCTION__);
+    return rv;
+}
+
+static bool can_talk_to_sensormanager()
+{
+    android::SensorManager& sensorManager(
+            android::SensorManager::getInstanceForPackage(android::String16("camera")));
+    android::Sensor const * const * sensorList;
+    return sensorManager.getSensorList(&sensorList) >= 0;
+}
+
+static char *camera_fixup_getparams(int id, const char *settings)
+{
+    android::CameraParameters params;
+    params.unflatten(android::String8(settings));
+
+#if !LOG_NDEBUG
+    ALOGV("%s: original parameters:", __FUNCTION__);
+    params.dump();
+#endif
+
+    params.set("scene-mode-values", "auto");
+    params.set("longshot-supported", "false");
+
+#if !LOG_NDEBUG
+    ALOGV("%s: fixed parameters:", __FUNCTION__);
+    params.dump();
+#endif
+
+    android::String8 strParams = params.flatten();
+    char *ret = strdup(strParams.string());
+
+    return ret;
+}
+
+static char *camera_fixup_setparams(int id, const char *settings)
+{
+    android::CameraParameters params;
+    params.unflatten(android::String8(settings));
+
+#if !LOG_NDEBUG
+    ALOGV("%s: original parameters:", __FUNCTION__);
+    params.dump();
+#endif
+
+#if !LOG_NDEBUG
+    ALOGV("%s: fixed parameters:", __FUNCTION__);
+    params.dump();
+#endif
+
+    android::String8 strParams = params.flatten();
+    if (fixed_set_params[id])
+        free(fixed_set_params[id]);
+    fixed_set_params[id] = strdup(strParams.string());
+    char *ret = fixed_set_params[id];
+
+    return ret;
+}
+
+/*******************************************************************
+ * implementation of camera_device_ops functions
+ *******************************************************************/
+
+static int camera_set_preview_window(struct camera_device *device,
+        struct preview_stream_ops *window)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, set_preview_window, window);
+}
+
+static void camera_set_callbacks(struct camera_device *device,
+        camera_notify_callback notify_cb,
+        camera_data_callback data_cb,
+        camera_data_timestamp_callback data_cb_timestamp,
+        camera_request_memory get_memory,
+        void *user)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return;
+
+    VENDOR_CALL(device, set_callbacks, notify_cb, data_cb, data_cb_timestamp,
+            get_memory, user);
+}
+
+static void camera_enable_msg_type(struct camera_device *device,
+        int32_t msg_type)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return;
+
+    VENDOR_CALL(device, enable_msg_type, msg_type);
+}
+
+static void camera_disable_msg_type(struct camera_device *device,
+        int32_t msg_type)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return;
+
+    VENDOR_CALL(device, disable_msg_type, msg_type);
+}
+
+static int camera_msg_type_enabled(struct camera_device *device,
+        int32_t msg_type)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return 0;
+
+    return VENDOR_CALL(device, msg_type_enabled, msg_type);
+}
+
+static int camera_start_preview(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, start_preview);
+}
+
+static void camera_stop_preview(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return;
+
+    VENDOR_CALL(device, stop_preview);
+}
+
+static int camera_preview_enabled(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, preview_enabled);
+}
+
+static int camera_store_meta_data_in_buffers(struct camera_device *device,
+        int enable)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, store_meta_data_in_buffers, enable);
+}
+
+static int camera_start_recording(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return EINVAL;
+
+    return VENDOR_CALL(device, start_recording);
+}
+
+static void camera_stop_recording(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return;
+
+    VENDOR_CALL(device, stop_recording);
+}
+
+static int camera_recording_enabled(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, recording_enabled);
+}
+
+static void camera_release_recording_frame(struct camera_device *device,
+        const void *opaque)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return;
+
+    VENDOR_CALL(device, release_recording_frame, opaque);
+}
+
+static int camera_auto_focus(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+
+    return VENDOR_CALL(device, auto_focus);
+}
+
+static int camera_cancel_auto_focus(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, cancel_auto_focus);
+}
+
+static int camera_take_picture(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, take_picture);
+}
+
+static int camera_cancel_picture(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, cancel_picture);
+}
+
+static int camera_set_parameters(struct camera_device *device,
+        const char *params)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    char *tmp = NULL;
+    tmp = camera_fixup_setparams(CAMERA_ID(device), params);
+
+    int ret = VENDOR_CALL(device, set_parameters, tmp);
+    return ret;
+}
+
+static char *camera_get_parameters(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return NULL;
+
+    char *params = VENDOR_CALL(device, get_parameters);
+
+    char *tmp = camera_fixup_getparams(CAMERA_ID(device), params);
+    VENDOR_CALL(device, put_parameters, params);
+    params = tmp;
+
+    return params;
+}
+
+static void camera_put_parameters(struct camera_device *device, char *params)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (params)
+        free(params);
+}
+
+static int camera_send_command(struct camera_device *device,
+        int32_t cmd, int32_t arg1, int32_t arg2)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, send_command, cmd, arg1, arg2);
+}
+
+static void camera_release(struct camera_device *device)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return;
+
+    VENDOR_CALL(device, release);
+}
+
+static int camera_dump(struct camera_device *device, int fd)
+{
+    ALOGV("%s->%08X->%08X", __FUNCTION__, (uintptr_t)device,
+            (uintptr_t)(((wrapper_camera_device_t*)device)->vendor));
+
+    if (!device)
+        return -EINVAL;
+
+    return VENDOR_CALL(device, dump, fd);
+}
+
+extern "C" void heaptracker_free_leaked_memory(void);
+
+/*******************************************************************
+ * implementation of camera_module functions
+ *******************************************************************/
+
+/* open device handle to one of the cameras
+ *
+ * assume camera service will keep singleton of each camera
+ * so this function will always only be called once per camera instance
+ */
+
+static int camera_get_number_of_cameras(void)
+{
+    ALOGV("%s", __FUNCTION__);
+    if (check_vendor_module())
+        return 0;
+    return gVendorModule->get_number_of_cameras();
+}
+
+static int camera_get_camera_info(int camera_id, struct camera_info *info)
+{
+    ALOGV("%s", __FUNCTION__);
+    if (check_vendor_module())
+        return 0;
+    return gVendorModule->get_camera_info(camera_id, info);
+}
+#endif
