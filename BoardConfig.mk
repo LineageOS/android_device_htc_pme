2ec3d9d09281ebda79dd94840cf5832c2175af11
Christopher R. Palmer <crpalmer@gmail.com>
2017-03-16 21:38:41 -0400

    pme: gps: Shim slim_daemon instead of hexediting it
    Change-Id: If29e6abd08c97f6f99c35657780a0298ad289351

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,6 +157,9 @@ TARGET_PROVIDES_KEYMASTER := true
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
+# Linker
+LINKER_FORCED_SHIM_LIBS := /system/vendor/bin/slim_daemon|liblog_htc.so
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
