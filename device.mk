b64f9a0d4495ecd7022b9fa505a7e5ccb5b6185e
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-15 12:45:16 +0200

    pme: Fail if hwui config is not available
    Change-Id: Iec82b053c9ffa31c5ffdadc442066cefe07ee064

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxxhdpi-4096-hwui-memory.mk)
+$(call inherit-product, frameworks/native/build/phone-xxxhdpi-4096-hwui-memory.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
